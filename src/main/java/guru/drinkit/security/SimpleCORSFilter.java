e66f9df1b933f29044470c91a160dac39c9e0fc1
Pavel Kolmykov <pavel.kolmykov@gmail.com>
2015-02-04 18:32:01 +0200

    added other access-control headers

--- src/main/java/guru/drinkit/security/SimpleCORSFilter.java
+++ src/main/java/guru/drinkit/security/SimpleCORSFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -15,16 +16,22 @@ import javax.servlet.http.HttpServletResponse;
 public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Expose-Headers", "WWW-Authenticate");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
+
+        if (request.getHeader("Access-Control-Request-Method") != null
+                && "OPTIONS".equals(request.getMethod())) {
+            // CORS "pre-flight" request
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
+        }
         chain.doFilter(req, res);
     }
 
-    public void init(FilterConfig filterConfig) {}
+    public void init(FilterConfig filterConfig) {
+    }
 
-    public void destroy() {}
+    public void destroy() {
+    }
 }
