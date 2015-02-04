ed7db9c91a6602d392a4745f1f5a07cc5e074938
Pavel Kolmykov <pavel.kolmykov@gmail.com>
2015-02-04 16:02:39 +0200

    added other access-control headers

--- src/main/java/guru/drinkit/security/SimpleCORSFilter.java
+++ src/main/java/guru/drinkit/security/SimpleCORSFilter.java
@@ -16,6 +16,7 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Expose-Headers", "WWW-Authenticate");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
