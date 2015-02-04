21e2d7202811255648b83a783d407a82f4bd6d8c
Pavel Kolmykov <pavel.kolmykov@gmail.com>
2015-02-04 17:32:18 +0200

    added other access-control headers

--- src/main/java/guru/drinkit/security/SimpleCORSFilter.java
+++ src/main/java/guru/drinkit/security/SimpleCORSFilter.java
@@ -1,30 +1,26 @@
 package guru.drinkit.security;
 
 import java.io.IOException;
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
  * Copy-pasted from https://spring.io/guides/gs/rest-service-cors/
  */
-public class SimpleCORSFilter implements Filter {
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
+public class SimpleCORSFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Expose-Headers", "WWW-Authenticate");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
-        chain.doFilter(req, res);
+        if (request.getHeader("Access-Control-Request-Method") != null
+                && "OPTIONS".equals(request.getMethod())) {
+            // CORS "pre-flight" request
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
+        }
     }
-
-    public void init(FilterConfig filterConfig) {}
-
-    public void destroy() {}
 }
