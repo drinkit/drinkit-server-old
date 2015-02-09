b9afb31d1149b05409cbf20ebcd62412131284cc
Pavel Kolmykov <pavel.kolmykov@gmail.com>
2015-02-09 17:44:48 +0200

    mybar with spring data rest [in progress]

--- src/main/java/guru/drinkit/springconfig/WebConfig.java
+++ src/main/java/guru/drinkit/springconfig/WebConfig.java
@@ -1,14 +1,16 @@
 package guru.drinkit.springconfig;
 
+import javax.annotation.Resource;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.annotation.Resource;
 
 /**
  * @author pkolmykov
@@ -19,6 +21,7 @@ import javax.annotation.Resource;
         "guru.drinkit.common"
 })
 @EnableWebMvc
+@Import(RepositoryRestMvcConfiguration.class)
 @EnableAspectJAutoProxy
 public class WebConfig extends WebMvcConfigurerAdapter {
 
