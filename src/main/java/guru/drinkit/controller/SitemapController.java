ecddf44535c4c617ea3cab60563bc20cc391833d
Crabar <crabar0@gmail.com>
2016-10-05 20:18:59 +0300

    added images to sitemap

--- src/main/java/guru/drinkit/controller/SitemapController.java
+++ src/main/java/guru/drinkit/controller/SitemapController.java
@@ -1,8 +1,8 @@
 package guru.drinkit.controller;
 
 
-import java.util.List;
-
+import cz.jiripinkas.jsitemapgenerator.ImageBuilder;
+import cz.jiripinkas.jsitemapgenerator.WebPage;
 import cz.jiripinkas.jsitemapgenerator.WebPageBuilder;
 import cz.jiripinkas.jsitemapgenerator.generator.SitemapGenerator;
 import guru.drinkit.domain.Recipe;
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 
 /**
@@ -28,7 +30,7 @@ public class SitemapController {
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
     @ResponseBody
     public String getSitemap() {
-        SitemapGenerator sitemapGenerator = new SitemapGenerator("https://drinkit.guru");
+        SitemapGenerator sitemapGenerator = new SitemapGenerator("https://drinkit.guru", new SitemapGenerator.AdditionalNamespace[]{SitemapGenerator.AdditionalNamespace.IMAGE});
         // const pages
         sitemapGenerator.addPage(new WebPageBuilder().build());
         sitemapGenerator.addPage(new WebPageBuilder().name("bar").build());
@@ -36,7 +38,11 @@ public class SitemapController {
         List<Recipe> recipes = recipeRepository.findAll();
         for (Recipe recipe : recipes) {
             if (recipe.getPublished()) {
-                sitemapGenerator.addPage(new WebPageBuilder().name("recipes/" + recipe.getId().toString()).build());
+                WebPage recipePage = new WebPageBuilder().name("recipes/" + recipe.getId().toString()).build();
+                recipePage.addImage(new ImageBuilder().loc("https://prod-drunkedguru.rhcloud.com/" + recipe.getImageUrl())
+                        .license("https://creativecommons.org/licenses/by-nc-nd/4.0/")
+                        .build());
+                sitemapGenerator.addPage(recipePage);
             }
         }
         return sitemapGenerator.constructSitemapString();
