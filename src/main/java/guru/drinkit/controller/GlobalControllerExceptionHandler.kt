42523b77b1c40910ad07427c13421463d9764c56
Pavel Kolmykov <pavel.kolmykov@gmail.com>
2016-12-11 23:43:49 +0200

    DRINKIT-42 rollbar (#37)

---
+++ src/main/java/guru/drinkit/controller/GlobalControllerExceptionHandler.kt
@@ -0,0 +1,45 @@
+package guru.drinkit.controller
+
+import com.rollbar.Rollbar
+import org.apache.commons.io.IOUtils
+import org.slf4j.LoggerFactory.getLogger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.annotation.AnnotationUtils
+import org.springframework.http.HttpStatus
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.nio.charset.Charset
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * @author pkolmykov
+ */
+@ControllerAdvice
+class GlobalControllerExceptionHandler
+@Autowired
+constructor(@Value("\${rollbar.token}") token: String, @Value("\${rollbar.env}") env: String) {
+    private val rollbar: Rollbar = Rollbar(token, env)
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleException(request: HttpServletRequest, e: RuntimeException) {
+        if (e is AccessDeniedException || AnnotationUtils.findAnnotation(e.javaClass, ResponseStatus::class.java) != null) {
+            throw e
+        }
+        getLogger(GlobalControllerExceptionHandler::class.java).error("Global exception", e)
+        logToRollbar(e, request)
+    }
+
+    private fun logToRollbar(e: RuntimeException, request: HttpServletRequest) {
+        rollbar.log(e, mapOf(
+                Pair("user", "${request.remoteUser} ${(request.userPrincipal as UsernamePasswordAuthenticationToken).authorities}"),
+                Pair("body", IOUtils.toString(request.inputStream, Charset.defaultCharset()))
+
+        ), "${request.method} ${request.requestURI}")
+    }
+
+}
